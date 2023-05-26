c99a9c70b07b853830fc0c6ee0d7dfd95a4c7fb1
Antoine Jeanson <antoine.j29@free.fr>
2023-05-26 14:01:05 +0200

    Add argument parsing

--- binmergetool/__main__.py
+++ binmergetool/__main__.py
@@ -1,17 +1,20 @@
 from binmergetool.binmergetool import BinMergeTool
 import argparse
+import struct
 
 if __name__ == "__main__":
     """
     Entry point when running the program as a module
     """
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument("file1", help="First file")
     parser.add_argument("file2", help="Second file")
     parser.add_argument("offset", help="Offset at which the second file should start")
-    parser.add_argument("padding_symbol", help="Padding symbol between file1 and file2")
-    parser.add_argument("outfile", help="Output file")
+    parser.add_argument(
+        "padding_symbol", help="Padding symbol between file1 and file2", default="0xFF"
+    )
+    parser.add_argument("outfile", help="Output file", default="merged.bin")
     args = parser.parse_args()
 
     with open(args.file1, "rb") as f:
@@ -20,9 +23,12 @@ if __name__ == "__main__":
     with open(args.file2, "rb") as f:
         data2 = f.read()
 
-    # TODO add padding symbol parsing
+    offset = int(args.offset, 16)
+    padding_symbol = struct.pack("B", int(args.padding_symbol, 16))
 
-    bmt = BinMergeTool(data1=data1, data2=data2, offset=int(args.offset))
+    bmt = BinMergeTool(
+        data1=data1, data2=data2, offset=offset, padding_symbol=padding_symbol
+    )
 
     with open(args.outfile, "wb") as f:
         f.write(bmt.merge())
